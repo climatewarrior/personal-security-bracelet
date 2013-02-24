318c29142cefd04ed131e1ba3633d82d52423823
Gabriel J. Pérez Irizarry <gabrieljoel@gmail.com>
2013-02-24 11:50:21 -0500

    Added arduino sketch

---
+++ PersonalSecurityBracelet/PersonalSecurityBracelet.ino
@@ -0,0 +1,34 @@
+/*
+  Blink
+  Turns on an LED on for one second, then off for one second, repeatedly.
+ 
+  This example code is in the public domain.
+ */
+ 
+// Pin 13 has an LED connected on most Arduino boards.
+// give it a name:
+const int buttonPin = 2;
+
+int buttonState = 0;
+
+// the setup routine runs once when you press reset:
+void setup() {                
+  // initialize the digital pin as an output.
+  pinMode(buttonPin, INPUT); 
+  digitalWrite (3, HIGH );
+
+
+  Serial.begin(9600);    
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  
+  buttonState = digitalRead(buttonPin);
+  
+  if (buttonState == LOW) {
+    Serial.println(7);
+    
+    
+  }
+}
