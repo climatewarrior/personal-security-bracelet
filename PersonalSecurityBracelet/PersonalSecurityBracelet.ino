c5c9371000b31ed1b0a878295b09f92f6b1f86b6
Gabriel J. Pérez Irizarry <gabrieljoel@gmail.com>
2013-03-23 14:16:04 -0400

    Separed old code into button example. Created bluetooth example. Added new sketch for actual project. Created new readme file for org-mode.

--- PersonalSecurityBracelet/PersonalSecurityBracelet.ino
+++ PersonalSecurityBracelet/PersonalSecurityBracelet.ino
@@ -1,34 +1,60 @@
 /*
   Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
+ Turns on an LED on for one second, then off for one second, repeatedly.
  
-  This example code is in the public domain.
+ This example code is in the public domain.
  */
- 
+
 // Pin 13 has an LED connected on most Arduino boards.
 // give it a name:
-const int buttonPin = 2;
+const int buttonPin = 7;
 
 int buttonState = 0;
+
+unsigned long time;
+unsigned long oldTime = 0;
+unsigned long timeDelta = 0;
+unsigned long threshold = 2500;
 
 // the setup routine runs once when you press reset:
 void setup() {                
   // initialize the digital pin as an output.
+  Serial.begin(115200);
   pinMode(buttonPin, INPUT); 
-  digitalWrite (3, HIGH );
+  digitalWrite(buttonPin, HIGH);     // sets the default (unpressed) state of switchPin to HIGH
 
 
-  Serial.begin(9600);    
+
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  
+
   buttonState = digitalRead(buttonPin);
-  
+
   if (buttonState == LOW) {
-    Serial.println(7);
+
+    if (oldTime > 0)
+    {
+      timeDelta += millis() - oldTime;
+      oldTime = millis();
+
+      if (timeDelta > threshold) 
+      {
+        Serial.println("Hello world!");
+        timeDelta = 0;
+        oldTime = 0;
+      }
+    }
     
-    
+    oldTime = millis();
+
+  }
+
+  else {
+
+    timeDelta = 0;
   }
 }
+
+
--- README.md
+++
@@ -1,2 +0,0 @@
-personal-security-bracelet
-==========================
