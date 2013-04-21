239c0e48c529272dcb72429884c4126dc63b1927
Gabriel J. Pérez Irizarry <gabrieljoel@gmail.com>
2013-04-21 18:10:35 -0400

    Adds emergency led, fixes click detection.

--- PersonalSecurityBracelet/PersonalSecurityBracelet.ino
+++ PersonalSecurityBracelet/PersonalSecurityBracelet.ino
@@ -1,101 +1,72 @@
-/*
-  Blink
- Turns on an LED on for one second, then off for one second, repeatedly.
- 
- This example code is in the public domain.
- */
+#include "ClickButton.h"
 
-// Pin 13 has an LED connected on most Arduino boards.
-// give it a name:
-const int buttonPin = 7;
+const int buttonPin = 3;
+const int led = 2;
+int ledState = 0;
+ClickButton button(buttonPin, LOW, CLICKBTN_PULLUP);
 
-int buttonState = 0;
+// Arbitrary LED function 
+int LEDfunction = 0;
+int oldLEDfunction = 0;
 
-unsigned long time;
-unsigned long oldTime = 0;
-unsigned long timeDelta = 0;
-unsigned long threshold = 2500;
-
-unsigned long clickTime;
-
-struct steps {
-  boolean one;
-  boolean two;
-  boolean three;
-};
-
-steps stepsvar = {false, false, false};
-
-boolean doubleclick = false;
-boolean clicked = false;
+boolean blink = false;
+boolean printed = false;
 
 // the setup routine runs once when you press reset:
 void setup() {                
-  // initialize the digital pin as an output.
   Serial.begin(115200);
-  pinMode(buttonPin, INPUT); 
-  digitalWrite(buttonPin, HIGH); // sets the default (unpressed) state of switchPin to HIGH
+
+  // Setup button timers (all in milliseconds / ms)
+  // (These are default if not set, but changeable for convenience)
+  button.maxPresses     = 2;    // max button multiclick count
+  button.debounceTime   = 20;   // Debounce timer in ms
+  button.multiclickTime = 250;  // Time limit for multi clicks
+  button.heldDownTime   = 1000; // time until "held-down clicks" register
+
+  pinMode(led, OUTPUT); 
+  digitalWrite(led, LOW);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
 
-  buttonState = digitalRead(buttonPin);
+  // Update button state
+  button.Update();
 
-  if (buttonState == LOW) {
+  oldLEDfunction = LEDfunction;
 
-    if (!clicked && stepsvar.two) {
+  // Save click codes in LEDfunction, as click codes are reset at next Update()
+  if (button.click != 0) LEDfunction = button.click;
 
-      doubleclick = true;
-    }
-
-    else {
-      clicked = true;
-      clickTime = millis();
-      doubleclick = false;
-      stepsvar.one = true;
-      stepsvar.two = false;
-      stepsvar.three = false;
-    }
-
-    if (oldTime > 0)
-    {
-      timeDelta += millis() - oldTime;
-      oldTime = millis();
-
-      if (timeDelta > threshold) 
-      {
-        Serial.println("EMERGENCY!");
-        timeDelta = 0;
-        oldTime = 0;
-      }
-    }
-    
-    oldTime = millis();
-
+  if (oldLEDfunction != LEDfunction) {
+      printed = false;
   }
 
-  else {
+  //Serial.println(LEDfunction);
 
-    if (clicked) {
-        stepsvar.two = true;
-	clicked = false;
-	doubleclick = false;
-    }
+  if(LEDfunction == 2) {
 
-    if (doubleclick) {
-      if (millis() - clickTime < 2000) {
-        Serial.println("CANCEL");
+      if (!printed) {
+	  Serial.println("CANCEL");
+	  printed = true;
       }
-      
-      clicked = false;
-      doubleclick = false;
-      stepsvar.one = false;
-      stepsvar.two = false;
-      stepsvar.three = false;
-    }
 
+      blink = false;
   }
+
+  if(LEDfunction == -1) {
+      if (!printed) {
+	  Serial.println("EMERGENCY");
+	  printed = true;
+      }
+      ledState = (millis()/2000)%2;
+      blink = true;
+  
+  }
+
+
+  if (blink) {
+      digitalWrite(led, ledState);
+  }
+
 }
-
-
